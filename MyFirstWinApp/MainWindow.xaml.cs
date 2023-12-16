df3ab5dcdb30e204292a698de9ed3fc3c0741228
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-12-16 22:28:47 +0300

    Add project files.

---
+++ MyFirstWinApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstWinApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonNumber_Click(object sender, RoutedEventArgs e)
+        {
+            TextboxResult.Text += ((Button)sender).Content;
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMultiplication_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDivision_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
